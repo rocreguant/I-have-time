d2791ff26916a37a546ccbaff7de75d455005f3f
Roc Reguant <roc.reguant@gmail.com>
2016-01-18 12:50:23 +0100

    finished, for short scope I win the market but in the long run don\'t

--- tendence-analysis.py
+++ tendence-analysis.py
@@ -14,6 +14,8 @@ __DATASET_FILES__ 		= [ '*CXBorsaDividends','*CXFonsTresorLlargTermini',
 __DATASET_EXTENSION__ = '.csv'
 __SAMPLE_LENGTH__		= 10
 
+me = 0
+market = 0 
 for _file in __DATASET_FILES__:
   f = open(__DATASET_FOLDER__+_file+__DATASET_EXTENSION__, 'r')
   X = np.ndarray(0)
@@ -26,10 +28,12 @@ for _file in __DATASET_FILES__:
     aux = row.split(';')
     X = np.append(X,aux[0][0:10])
     Y = np.append(Y,float(aux[1].replace(',','.')))
-
+  
+  print 'STARTING'
   #reversing the array    
   X = X[::-1]
   Y = Y[::-1]
+  Y = Y[len(Y)-101:len(Y)-1]
   
   l = []
   count = 0
@@ -37,20 +41,24 @@ for _file in __DATASET_FILES__:
   value = 0
   for i in range(len(Y)):
     try:
-      b = Y[i] <= Y[i+1]
+      b = Y[i] <= Y[i+1] #if grow
       if b:
+        if count == 0:
+          value = Y[i+2]
         count +=1
-        # value = Y[i+2] guess is correct
-      else:
+      else: #get lower
         l.append(count)
+        if count >0 : #Making sure that I actualy have 'bought' stuff
+          money = money * Y[i+2]/value
+        print money
         count = 0
     except:
-      print '\n'# out of range \n'
-
+      print '\n'# out of range \n Money: '
+      
+  m = 1000*Y[len(Y)-1]/Y[0]
+  print 'standard market: ',  m
+  print 'Algorithm outperformance: ', money/m-1
   counter=collections.Counter(l)
   print counter
   print len(l)-counter[0]-counter[1]
-  
-  
-  #check the market increase
-  #check my increase considering that I loose one day of trading and sell at the end of the following day
+
